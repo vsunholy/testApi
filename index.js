9ab632850f6d3a2cace154eeabeb3ee458b5a262
vsunholy <vitalijsnopov@gmail.com>
2025-02-04 09:56:24 +0200

    Add CRUD operations for users in index.js

--- index.js
+++ index.js
@@ -57,7 +57,50 @@ app.get('/users', async (req, res) => {
 
 
 
+app.get('/users/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+        res.status(200).json(result.rows);
+    }
+    catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+})
 
+app.post('/users/create', async (req, res) => {
+    try {
+        const { username, password} = req.body; 
+        const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [username, password]);
+        res.status(200).json(result.rows);
+    }
+    catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+});
+
+app.put('/users/update/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { username, password } = req.body;
+        const result = await pool.query('UPDATE users SET username = $1, password = $2 WHERE id = $3 RETURNING *', [username, password, id]);
+        res.status(200).json(result.rows);
+    }
+    catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+});
+
+app.delete('/users/delete/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('DELETE FROM users WHERE id = $1 RETURNING *', [id]);
+        res.status(200).json(result.rows);
+    }
+    catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+});
 
 
 //end game things
