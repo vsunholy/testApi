7c0b221f8921236e80e8e1bdf3fb013d80e9c456
vsunholy <vitalijsnopov@gmail.com>
2025-02-11 10:08:14 +0200

    Clean up whitespace in ToDo test file for improved readability

--- cypress/e2e/ToDoJamesTests/todojames.cy.js
+++ cypress/e2e/ToDoJamesTests/todojames.cy.js
@@ -1,18 +1,18 @@
 
 
 describe('ToDoTest', () => {
- 
+
     it('Create a new ToDo', () => {
         cy.visit('https://todolist.james.am/#/');
         cy.get('input.new-todo').type('1 uzduotis{enter}');
         expect(cy.get('ul.todo-list').contains('1 uzduotis')).to.exist;
 
-      
 
-        
+
+
     })
 
-   
-       
- 
+
+
+
 });
