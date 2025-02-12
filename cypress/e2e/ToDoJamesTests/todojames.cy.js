050fbaad73872c703cbe49845e5c85f1b6a05800
vsunholy <vitalijsnopov@gmail.com>
2025-02-12 10:00:10 +0200

    Add Cypress tests for deleting and editing ToDo items

--- cypress/e2e/ToDoJamesTests/todojames.cy.js
+++ cypress/e2e/ToDoJamesTests/todojames.cy.js
@@ -1,18 +1,47 @@
-
-
 describe('ToDoTest', () => {
 
     it('Create a new ToDo', () => {
         cy.visit('https://todolist.james.am/#/');
         cy.get('input.new-todo').type('1 uzduotis{enter}');
-        expect(cy.get('ul.todo-list').contains('1 uzduotis')).to.exist;
-
-
-
-
+        expect(cy.get('ul.todo-list')
+            .contains('1 uzduotis'))
+            .to.exist;
     })
 
+    it('Delete a ToDo', () => {
+        cy.visit('https://todolist.james.am/#/');
+        cy.get('input.new-todo').type('1 uzduotis{enter}');
+        cy.get('input.new-todo').type('2 uzduotis{enter}');
+
+        cy.contains('2 uzduotis')
+            .parent()
+            .find('button.destroy')
+            .invoke('show')
+            .click();
 
 
+        cy.contains('ul.todo-list li', '2 uzduotis')
+            .should('not.exist');
+        cy.contains('ul.todo-list li', '1 uzduotis')
+            .should('exist');
+    });
+
+
+    it('Edit a ToDo', () => {
+        cy.visit('https://todolist.james.am/#/');
+        cy.get('input.new-todo').type('1 uzduotis{enter}');
+        cy.get('input.new-todo').type('2 uzduotis{enter}');
+        cy.get('input.new-todo').type('3 uzduotis{enter}');
+
+
+        cy.contains('ul.todo-list li', '2 uzduotis')
+            .dblclick()
+            .find('input.edit')
+            .clear()
+            .type('redaguota uzduotis{enter}')
+            .should('exist');
+
+            
+    });
 
 });
