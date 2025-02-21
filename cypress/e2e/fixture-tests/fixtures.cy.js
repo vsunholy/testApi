bc39a2560597ae84c8b1ec2ec273d59647d8932e
vsunholy <vitalijsnopov@gmail.com>
2025-02-21 09:56:30 +0200

    Add Cypress tests for generating and verifying 100 ToDo items using fixtures

--- cypress/e2e/fixture-tests/fixtures.cy.js
+++ cypress/e2e/fixture-tests/fixtures.cy.js
@@ -3,7 +3,9 @@
 
 describe('Fixtures works', () => {
 
-
+    beforeEach(() => {
+        cy.visit('https://todolist.james.am/#/')
+    });
 
 
     it('fixtures test', () => {
@@ -29,15 +31,53 @@ describe('Fixtures works', () => {
 
     });
 
+    // it('Duomenu generavimas', () =>{
+    //     let tasks = [];
+    //     for (let i = 1; i <= 100; i++) {
+    //         tasks.push(i+ " uzduotis");
+
+    //     }
+    //     console.log(tasks)
+    //     cy.writeFile('cypress/fixtures/todos.json', {"todos": tasks});
+    // })
 
 
 
+    // it.only('100 tasks on the toDoJames website', () => {
+    //     cy.visit('https://todolist.james.am/#/');
+    //     for (let i = 0; i <= 100; i++) {
+    //         cy.get('input.new-todo').type(i + ' užduotis{enter}');
+    //     }
+    //     cy.contains('ul.todo-list li', '1 užduotis').should('be.visible');
+
+    // })
 
 
+    it.only('100 tasks on the toDoJames website', () => {
 
+        cy.session('fixture sessija', () => {
+            cy.visit('https://todolist.james.am/#/')
+            cy.fixture('todos').as('TodosJson').then((todosFile) => {
+                cy.log(todosFile)
+                cy.log(todosFile.todos[54])
+                // todosFile.todos.forEach((todo) => {
+                //     cy.get('input.new-todo').type(todo + '{enter}');
+                // });
 
+                cy.wrap(todosFile.todos).each((todo) => {
+                    cy.get('input.new-todo').type(todo + '{enter}');
+                });
 
+            })
+        })
+        cy.visit('https://todolist.james.am/#/')
+        cy.get('.todo-list li').first()
+            .should('be.visible');
+        cy.get('.todo-list li').last()
+            .should('be.visible');
+        cy.get('ul.todo-list li').should('have.length', 100);
 
+    });
 
 
 
